2bcb5a752130ae9f7e82c5927bb81ee51495e90f
Karan <Karanelisha@gmail.com>
2025-09-04 20:51:27 +0300

    Feat: Added photo details editting. Add: +Button to submit the data +Method to get images by id and album id +Method to patch data after editting +Added form controls and validators Styles: Added input styles Test: +Works as expected

---
+++ src/app/photos.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Photos } from './photo';
+import { EMPTY, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  // variable to store the link for fetching data
+  photosUrl: string = "https://jsonplaceholder.typicode.com/photos";
+
+  constructor(private httpClient: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  // method to get images by album Id. 
+  getPhotosByAlbumId(albumId: number): Observable<Photos[]>{
+    return this.httpClient.get<Photos[]>(`${this.photosUrl}?albumId=${albumId}`);
+  }
+
+  // This method gets images by Id.
+  getImage(id: number): Observable<Photos> {
+    return this.httpClient.get<Photos>(`${this.photosUrl}/${id}`);
+  }
+
+  // This method patches is used to edit photo details and posts to the server
+  updateImage(id: number, imageData: Photos): Observable<Photos> {
+    return this.httpClient.patch<Photos>(`${this.photosUrl}/${id}`, imageData);
+  }
+
+}
